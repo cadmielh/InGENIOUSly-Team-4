cfde8c6ff5dd201c480a1d896fcc585c5e8648f2
cadmielh <cadmielh@yahoo.com>
2016-03-23 11:52:40 +0200

    #5 - USART communicating problem solved

--- Software/GccApplication1/GccApplication1/Debug/main.d
+++ Software/GccApplication1/GccApplication1/Debug/main.d
@@ -1,9 +1,13 @@
 main.d main.o: .././main.cpp \
- d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
- d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\delay.h \
+ d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
  d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
  d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdint.h \
  d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
+ d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
  D:\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.0.98\include/avr/iom1284.h \
  d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
  d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
@@ -11,15 +15,23 @@ main.d main.o: .././main.cpp \
  d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
  d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h
 
-d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\delay.h:
 
-d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
 
 d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
 
 d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdint.h:
 
 d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+
+d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+d:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
 
 D:\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.0.98\include/avr/iom1284.h:
 
--- Software/GccApplication1/GccApplication1/main.cpp
+++ Software/GccApplication1/GccApplication1/main.cpp
@@ -1,143 +1,83 @@
-
-
-
- 
-#include <avr/io.h> 
-//#include <avr/interrupt.h>
-
 #define F_CPU 20000000ul	// 20 MHz oscillator.
-#define BaudRate 9600
-#define MYUBRR (uint8_t)(F_CPU / 20 / BaudRate )
-#define BaudRate 9600
+#define BAUD 9600
+#define MYUBRR 130
 
 
- 
-
-
-
-void delayLong()
-{
-	unsigned int delayvar;
-	delayvar = 0; 
-	while (delayvar <=  65500U)		
-	{ 
-		asm("nop");  
-		delayvar++;
-	} 
-}
-
-
-uint8_t serialCheckRxComplete(void)
-{
-	
-	return( UCSR0A & _BV(RXC0)) ;		// nonzero if serial data is available to read.
-}
-
-uint8_t serialCheckTxReady(void)
-{
-	return( UCSR0A & _BV(UDRE0) ) ;		// nonzero if transmit register is ready to receive new data.
-}
-
-uint8_t serialRead(void)
-{
-	while (serialCheckRxComplete() == 0)		// While data is NOT available to read
-	{;;} 
-	
-	return UDR0;
-}
-
-void serialWrite(uint8_t DataOut)
-{
-	while (serialCheckTxReady() == 0)		// while NOT ready to transmit 
-	{;;} 
-	UDR0 = DataOut;
-}
+#include <avr/delay.h>
+#include <avr/io.h> 
 
 
 
-void establishContact() {
-	while (serialCheckRxComplete() == 0) { 
-		//serialWrite('A');
-	//	serialWrite(65U);
-		delayLong();
-		delayLong();
-		delayLong();
-		delayLong();
-		delayLong();
-		delayLong();
-		delayLong();
-	}
-}
 
-  
+void USART_Init();
+void USART_Transmit( unsigned char data );
+unsigned char USART_Receive( void );
+void PWM_init();
+unsigned int my_ubrr = MYUBRR;
+unsigned char speed;
+
 
 int main (void)
 { 
-	
-//Interrupts are not needed in this program. You can optionally disable interrupts.	
-//asm("cli");		// DISABLE global interrupts.
-
-		
-	DDRD = _BV(1);
-	DDRB = _BV(0) | _BV(1) | _BV(3) | _BV(5);
+	USART_Init();
+	 PWM_init();
 	 
-	//Serial Initialization
-	
- 	/*Set baud rate */ 
-	UBRR0H = (uint8_t)(MYUBRR>>8); 
-	UBRR0L = (uint8_t) MYUBRR; 
-	/* Enable receiver and transmitter   */
-	UCSR0B = (1<<RXEN0)|(1<<TXEN0); 
-	/* Frame format: 8data, No parity, 1stop bit */ 
-	UCSR0C = (3<<UCSZ00);  
+	 
 	
 	 
-	int firstSensor = 0;    // first analog sensor
-	int secondSensor = 0;   // second analog sensor
-	int thirdSensor = 0;    // digital sensor
-	volatile uint8_t inByte = 0;         // incoming serial byte
-	
-	
-	PORTB |= _BV(1); // Turn on LED @ PB1
-	
-
-//establishContact();  // send a byte to establish contact until Processing responds 
-
-		PORTB &= 253U; // Turn off LED
-	
-
-for (;;)  // main loop										
-{
-
-	
-	if (serialCheckRxComplete()) {
-		PORTB |= _BV(1); // Turn on LED @ PB1
+	while (1)  // main loop										
+	{
 		
-		inByte = serialRead();
-		//inByte &= 0x7fu;
-		
-		UDR0 = 0x00u;
-		
-		// Simulated data!
-		firstSensor++;
-		
-		secondSensor = firstSensor * firstSensor;
-		
-		thirdSensor = firstSensor + secondSensor;
-		
-		//serialWrite(firstSensor & 255U);
-		//serialWrite(secondSensor & 255U);
-		//serialWrite(thirdSensor & 255U);
-
-		PORTB &= 253U; // Turn off LED
-              
-	}
-	
-	
-	 	
-	
-
-}	//End main loop.
-	return 0;
+		speed = USART_Receive();
+		if(speed != 0)
+		 {
+				OCR0A=speed;
+				USART_Transmit(speed);
+		 }
+	} 
+return 0;
 }
 
+
+void PWM_init()
+{
+	// initialize TCCR0 as per requirement, say as follows
+	TCCR0A |= (1<<COM0A1)|(1<<WGM00);
+	TCCR0B = (1<<CS01);
+	
+	//DDRD|=(1<<PORTD5);
+	//PWM PIN
+	DDRB|= (1<<PORTB3);
+}
+
+
+
+void USART_Init()
+{
+	/* Set baud rate */
+	UBRR0H = (unsigned char)(my_ubrr>>8);
+	UBRR0L = (unsigned char)my_ubrr;
+	/* Enable receiver and transmitter */
+	UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+	/* Set frame format: 8data, 2stop bit */
+	UCSR0C = (1<<USBS0)|(3<<UCSZ00);
+}
+
+void USART_Transmit( unsigned char data )
+{
+	/* Wait for empty transmit buffer */
+	while ( !( UCSR0A & (1<<UDRE0)) )
+	;
+	/* Put data into buffer, sends the data */
+	UDR0 = data;
+}
+
+
+unsigned char USART_Receive( void )
+{
+	/* Wait for data to be received */
+	while ( !(UCSR0A & (1<<RXC0)) )
+	;
+	/* Get and return received data from buffer */
+	return UDR0;
+}
